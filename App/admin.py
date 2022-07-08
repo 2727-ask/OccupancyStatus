c6baf2a23d1e8ef56acd5212845e4b0de99459ac
AshutoshKumbharMindsClick27 <ashutosh.kumbhar@mindsclik.com>
2022-07-08 22:29:08 +0530

    s

--- App/admin.py
+++ App/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Doctors,Hospital,Owner,Feature,UserDetails,Booking,UserInformation,HospitalInformation,HospitalSocialInformation
+from .models import Doctors,Hospital,Owner,Feature,UserDetails,Booking,UserInformation,HospitalInformation,HospitalSocialInformation,HospitalImages
 # Register your models here.
 admin.site.register(Doctors)
 admin.site.register(Hospital)
@@ -10,4 +10,5 @@ admin.site.register(Booking)
 admin.site.register(UserInformation)
 admin.site.register(HospitalInformation)
 admin.site.register(HospitalSocialInformation)
+admin.site.register(HospitalImages)
 
